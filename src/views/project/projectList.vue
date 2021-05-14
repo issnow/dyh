318ef8035feb0520a523f53f42356cd544aa9736
li.weifeng <weifeng_li@moviebook.cn>
2021-05-14 09:36:04 +0800

    l列表页

--- src/views/project/projectList.vue
+++ src/views/project/projectList.vue
@@ -1,64 +1,52 @@
 <template>
   <div>
     <div class="main-wrap">
-      <el-row class="mb30">
-        <el-col :span="6">
-          搜索：<el-input placeholder="请输入作品名称关键字" v-model="searchValue" class="work-name"></el-input>
-        </el-col>
-        <el-col :span="6">
-        状态：
-        <el-select v-model="status" placeholder="请选择">
-          <el-option
-            v-for="item in selectData"
-            :key="item.value"
-            :label="item.label"
-            :value="item.value">
-          </el-option>
-        </el-select>
-        </el-col>
-        <el-col :span="6">
-          <el-button type="primary">查询</el-button>
-        </el-col>
-      </el-row>
+      <el-form
+        :model="form"
+        :rules="rules"
+        ref="form"
+        label-width="60px"
+        :inline="true">
+        <el-row>
+          <el-col :span="7">
+            <el-form-item label="搜索" prop="keyword">
+              <el-input v-model="form.keyword" placeholder="请输入作品名称关键字"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="7">
+            <el-form-item label="状态" prop="status">
+              <el-select v-model="form.status" placeholder="请选择">
+                <el-option
+                  v-for="item in selectData"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value">
+                </el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="2">
+            <el-form-item>
+              <el-button type="primary" @click="submitForm('form')">查询</el-button>
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </el-form>
 
-      <el-table
-        class="mb20"
-        :data="tableData"
-        border
-        style="width: 100%">
-        <el-table-column
-          prop="date"
-          label="作品名称">
-        </el-table-column>
-        <el-table-column
-          prop="name"
-          label="创作人"
-          width="200">
-        </el-table-column>
-        <el-table-column
-          prop="province"
-          label="状态"
-          width="100">
-        </el-table-column>
-        <el-table-column
-          prop="city"
-          label="联系人"
-          width="100">
-        </el-table-column>
-        <el-table-column
-          prop="address"
-          label="状态"
-          width="120">
-        </el-table-column>
-        <el-table-column
-          prop="zip"
-          label="创作时间"
-          width="200">
-        </el-table-column>
-        <el-table-column
-          fixed="right"
-          label="操作">
+      <el-table v-loading='loading' class="mb20" :data="tableData" border style="width: 100%">
+        <el-table-column prop="date" label="作品名称"></el-table-column>
+        <el-table-column prop="name" label="创作人" width="200"></el-table-column>
+        <el-table-column prop="province" label="状态" width="100"></el-table-column>
+        <el-table-column prop="city" label="联系人" width="100"></el-table-column>
+        <el-table-column prop="address" label="状态" width="120"></el-table-column>
+        <el-table-column prop="zip" label="创作时间" width="200"></el-table-column>
+        <el-table-column fixed="right" label="操作">
           <template slot-scope="scope">
+              <!-- 视频组件 -->
+                <!-- <preview
+                  :bgImage="scope.row.small_preview_path"
+                  :source="scope.row.address_original"
+                /> -->
             <el-button type="text" @click="onReview">提交审核</el-button>
             <el-button type="text" @click="onEdit">编辑</el-button>
             <el-button type="text" @click="omDelete">删除</el-button>
@@ -68,6 +56,7 @@
       </el-table>
 
       <el-pagination
+        background
         @size-change="handleSizeChange"
         @current-change="handleCurrentChange"
         :current-page="currentPage"
@@ -87,6 +76,19 @@
   export default {
     data() {
       return {
+        loading: false,
+        form: {
+          keyword: "",
+          status: "",
+        },
+        rules: {
+          keyword: [
+            { required: true, message: "请输入作品名称关键字", trigger: "blur" },
+          ],
+          status: [
+            { required: true, message: "请选择状态", trigger: "change" },
+          ],
+        },
         selectData: [{
           value: '1',
           label: '合成中'
@@ -158,6 +160,20 @@
     components: {
     },
     methods: {
+      onReview(){},
+      onEdit(){},
+      omDelete(){},
+      onDownload(){},
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (!valid) {
+            console.log("error submit!!");
+            return false;
+          }
+          // 请求接口
+          console.log(this.form, "form");
+        });
+      },
       handleSizeChange(val) {
         console.log(`每页 ${val} 条`);
       },
--- src/views/work/manager.vue
+++ src/views/work/manager.vue
@@ -1,75 +1,49 @@
 <template>
   <div>
     <div class="main-wrap">
-      <el-row class="mb30">
-        <el-col :span="6">
-          搜索：<el-input placeholder="请输入作品名称关键字" v-model="searchValue" class="work-name"></el-input>
-        </el-col>
-        <el-col :span="6">
-        状态：
-        <el-select v-model="status" placeholder="请选择">
-          <el-option
-            v-for="item in selectData"
-            :key="item.value"
-            :label="item.label"
-            :value="item.value">
-          </el-option>
-        </el-select>
-        </el-col>
-        <el-col :span="6">
-          <el-button type="primary">查询</el-button>
-        </el-col>
-      </el-row>
+      <el-form
+        :model="form"
+        :rules="rules"
+        ref="form"
+        label-width="60px"
+        :inline="true">
+        <el-row>
+          <el-col :span="7">
+            <el-form-item label="搜索" prop="keyword">
+              <el-input v-model="form.keyword" placeholder="请输入作品名称关键字"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="7">
+            <el-form-item label="状态" prop="status">
+              <el-select v-model="form.status" placeholder="请选择">
+                <el-option
+                  v-for="item in selectData"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value">
+                </el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="2">
+            <el-form-item>
+              <el-button type="primary" @click="submitForm('form')">查询</el-button>
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </el-form>
 
-      <el-table
-        class="mb20"
-        :data="tableData"
-        border
-        style="width: 100%">
-        <el-table-column
-          prop="date"
-          label="作品名称">
-        </el-table-column>
-        <el-table-column
-          prop="name"
-          label="创作人">
-        </el-table-column>
-        <el-table-column
-          prop="province"
-          label="类型"
-          width="80">
-        </el-table-column>
-        <el-table-column
-          prop="city"
-          label="画幅"
-          width="100">
-        </el-table-column>
-        <el-table-column
-          prop="address"
-          label="大小"
-          width="120">
-        </el-table-column>
-        <el-table-column
-          prop="zip"
-          label="时长"
-          width="80">
-        </el-table-column>
-        <el-table-column
-          prop="zip"
-          label="状态"
-          width="100">
-        </el-table-column>
-        <el-table-column
-          prop="zip"
-          label="创作时间"
-          width="200">
-        </el-table-column>
-        <el-table-column
-          fixed="right"
-          label="操作">
+      <el-table v-loading='loading' class="mb20" :data="tableData" border style="width: 100%">
+        <el-table-column prop="date" label="作品名称"></el-table-column>
+        <el-table-column prop="name" label="创作人" width="200"></el-table-column>
+        <el-table-column prop="province" label="状态" width="100"></el-table-column>
+        <el-table-column prop="city" label="联系人" width="100"></el-table-column>
+        <el-table-column prop="address" label="状态" width="120"></el-table-column>
+        <el-table-column prop="zip" label="创作时间" width="200"></el-table-column>
+        <el-table-column fixed="right" label="操作">
           <template slot-scope="scope">
             <el-button type="text" @click="onReview">提交审核</el-button>
-            <el-button type="text" @click="onGenerate">编辑</el-button>
+            <el-button type="text" @click="onGenerate">重新合成</el-button>
             <el-button type="text" @click="omDelete">删除</el-button>
             <el-button type="text" @click="onWatch">查看</el-button>
           </template>
@@ -77,6 +51,7 @@
       </el-table>
 
       <el-pagination
+        background
         @size-change="handleSizeChange"
         @current-change="handleCurrentChange"
         :current-page="currentPage"
@@ -96,6 +71,19 @@
   export default {
     data() {
       return {
+        loading: false,
+        form: {
+          keyword: "",
+          status: "",
+        },
+        rules: {
+          keyword: [
+            { required: true, message: "请输入作品名称关键字", trigger: "blur" },
+          ],
+          status: [
+            { required: true, message: "请选择状态", trigger: "change" },
+          ],
+        },
         selectData: [{
           value: '1',
           label: '合成中'
@@ -167,6 +155,20 @@
     components: {
     },
     methods: {
+      onReview(){},
+      onGenerate(){},
+      omDelete(){},
+      onWatch(){},
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (!valid) {
+            console.log("error submit!!");
+            return false;
+          }
+          // 请求接口
+          console.log(this.form, "form");
+        });
+      },
       handleSizeChange(val) {
         console.log(`每页 ${val} 条`);
       },
