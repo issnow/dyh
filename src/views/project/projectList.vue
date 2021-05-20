7a656f3db1eae3c96ae885b83be911f0c7e479cc
li.weifeng <weifeng_li@moviebook.cn>
2021-05-20 16:34:33 +0800

    项目管理接口添加

--- src/views/project/projectList.vue
+++ src/views/project/projectList.vue
@@ -4,30 +4,20 @@
         :model="form"
         :rules="rules"
         ref="form"
-        label-width="60px"
-        :inline="true">
+        label-width="60px">
         <el-row>
-          <el-col :span="7">
+          <el-col :span="8">
             <el-form-item label="搜索" prop="keyword">
-              <el-input v-model="form.keyword" placeholder="请输入关键字"></el-input>
+              <el-input v-model="form.keyword" placeholder="请输入项目名称关键字"><el-button slot="append" icon="el-icon-search"></el-button></el-input>
             </el-form-item>
           </el-col>
-          <el-col :span="7">
-            <el-form-item label="状态" prop="status">
-              <el-select v-model="form.status" placeholder="请选择">
-                <el-option
-                  v-for="item in selectData"
-                  :key="item.value"
-                  :label="item.label"
-                  :value="item.value">
-                </el-option>
-              </el-select>
-            </el-form-item>
+          <el-col :span="4">
+              <el-button type="primary" @click="resetSearch">重置</el-button>
           </el-col>
-          <el-col :span="2">
-            <el-form-item>
-              <el-button type="primary" @click="submitForm('form')">查询</el-button>
-            </el-form-item>
+
+          <el-col :span="12" class="text-right">
+            <el-button type="primary" icon="el-icon-plus" @click="isShow = true">创建项目</el-button>
+            <el-button type="primary" icon="el-icon-delete" @click="deleteProject">批量删除</el-button>
           </el-col>
         </el-row>
       </el-form>
@@ -39,24 +29,21 @@
         border
         style="width: 100%"
         :default-sort = "{prop: 'zip', order: 'descending'}"
+        @selection-change="handleSelectionChange"
         >
-        <el-table-column prop="date" label="作品名称"></el-table-column>
-        <el-table-column prop="name" label="创作人" width="200"></el-table-column>
-        <el-table-column prop="province" label="类型" width="100"></el-table-column>
-        <el-table-column prop="city" label="联系人" width="100"></el-table-column>
-        <el-table-column prop="address" label="状态" width="120"></el-table-column>
-        <el-table-column prop="zip" label="创作时间" width="200" sortable></el-table-column>
+        <el-table-column type="selection" width="55"></el-table-column>
+        <el-table-column prop="date" label="项目名称"></el-table-column>
+        <el-table-column prop="name" label="类型" width="100"></el-table-column>
+        <el-table-column prop="province" label="分辨率" width="100"></el-table-column>
+        <el-table-column prop="city" label="画幅" width="100"></el-table-column>
+        <!-- <el-table-column prop="address" label="大小（M）" width="120"></el-table-column> -->
+        <!-- <el-table-column prop="zip" label="时长（S）" width="200" sortable></el-table-column> -->
+        <el-table-column prop="zip" label="创建时间" width="200" sortable></el-table-column>
+        <el-table-column prop="zip" label="更新时间" width="200" sortable></el-table-column>
         <el-table-column fixed="right" label="操作">
           <template slot-scope="scope">
-              <!-- 视频组件 -->
-                <!-- <preview
-                  :bgImage="scope.row.small_preview_path"
-                  :source="scope.row.address_original"
-                /> -->
-            <el-button type="text" @click="onReview">提交审核</el-button>
             <el-button type="text" @click="onEdit">编辑</el-button>
-            <el-button type="text" @click="omDelete">删除</el-button>
-            <el-button type="text" @click="onDownload">下载</el-button>
+            <el-button type="text" @click="omDelete(scope)">删除</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -72,208 +59,71 @@
         :total="50">
       </el-pagination>
 
-      <!-- 提交审核 -->
-      <el-dialog title="提交审核" :visible="isShow" :before-close="handleClose">
-        <el-form :model="viiewForm" label-width="100px" :rules="viiewFormRules" ref="viiewForm">
-          <el-form-item label="名称">
-            <el-input v-model="viiewForm.name" disabled></el-input>
+      <!-- 删除提示 -->
+      <el-dialog
+        title="删除确认"
+        :visible="isDelete"
+        width="30%"
+        :before-close="handleClose">
+        <span>请确认是否删除该项目？</span>
+        <span slot="footer" class="dialog-footer">
+          <el-button @click="isDelete = false">取 消</el-button>
+          <el-button type="primary" @click="deleteProject">确 认</el-button>
+        </span>
+      </el-dialog>
+
+      <!-- 创建项目 -->
+      <el-dialog 
+        title="创建项目"
+        :visible="isShow"
+        :before-close="handleClose"
+        class="creat-dialog"
+        width="30%">
+        <el-form :model="creatForm" label-width="80px" :rules="creatFormRules" ref="creatForm">
+          <el-form-item label="项目名称" prop="name">
+            <el-input 
+              v-model="creatForm.name"
+              placeholder="请输入项目名称"
+              maxlength="10"
+              show-word-limit
+            >
+            </el-input>
           </el-form-item>
-          <el-form-item label="描述">
-            <el-input v-model="viiewForm.desc" autocomplete="off" placeholder="请输入0-20个字符的描述"></el-input>
-          </el-form-item>
-          <el-form-item label="标签">
-            <el-select v-model="viiewForm.label" placeholder="请选择活动区域">
+          <el-form-item label="画幅比例" prop="scale">
+            <el-select v-model="creatForm.scale" placeholder="请选择活动区域">
               <el-option label="区域一" value="shanghai"></el-option>
               <el-option label="区域二" value="beijing"></el-option>
             </el-select>
           </el-form-item>
-          <el-form-item label="实体">
-            <el-select v-model="viiewForm.entity1" placeholder="请选择活动区域">
+          <el-form-item label="分辨率" prop="size">
+            <el-select v-model="creatForm.size" placeholder="请选择活动区域">
               <el-option label="区域一" value="shanghai"></el-option>
               <el-option label="区域二" value="beijing"></el-option>
             </el-select>
-            <el-select v-model="viiewForm.entity2" placeholder="请选择活动区域">
-              <el-option label="区域一" value="shanghai"></el-option>
-              <el-option label="区域二" value="beijing"></el-option>
-            </el-select>
-          </el-form-item>
-          <el-form-item label="实体">
-            <el-cascader
-              v-model="viiewForm.entity3"
-              :options="options"
-              @change="handleChange">
-            </el-cascader>
           </el-form-item>
         </el-form>
         <div slot="footer" class="dialog-footer">
           <el-button @click="isShow = false">取 消</el-button>
-          <el-button type="primary" @click="onConfirm">确 定</el-button>
+          <el-button type="primary" @click="onConfirm('creatForm')">确 认</el-button>
         </div>
       </el-dialog>
     </div>
 </template>
 
 <script>
-  // import {
-  //   getData
-  // } from '@api/main'
+  import { 
+    getChoicesList,
+    createProject,
+    delProject,
+    getProjectList 
+  } from "@api/project";
+
   export default {
     data() {
       return {
-        options: [{
-          value: 'zhinan',
-          label: '指南',
-          children: [{
-            value: 'shejiyuanze',
-            label: '设计原则',
-            children: [{
-              value: 'yizhi',
-              label: '一致'
-            }, {
-              value: 'fankui',
-              label: '反馈'
-            }, {
-              value: 'xiaolv',
-              label: '效率'
-            }, {
-              value: 'kekong',
-              label: '可控'
-            }]
-          }, {
-            value: 'daohang',
-            label: '导航',
-            children: [{
-              value: 'cexiangdaohang',
-              label: '侧向导航'
-            }, {
-              value: 'dingbudaohang',
-              label: '顶部导航'
-            }]
-          }]
-        }, {
-          value: 'zujian',
-          label: '组件',
-          children: [{
-            value: 'basic',
-            label: 'Basic',
-            children: [{
-              value: 'layout',
-              label: 'Layout 布局'
-            }, {
-              value: 'color',
-              label: 'Color 色彩'
-            }, {
-              value: 'typography',
-              label: 'Typography 字体'
-            }, {
-              value: 'icon',
-              label: 'Icon 图标'
-            }, {
-              value: 'button',
-              label: 'Button 按钮'
-            }]
-          }, {
-            value: 'form',
-            label: 'Form',
-            children: [{
-              value: 'radio',
-              label: 'Radio 单选框'
-            }, {
-              value: 'input',
-              label: 'Input 输入框'
-            }, {
-              value: 'input-number',
-              label: 'InputNumber 计数器'
-            }, {
-              value: 'select',
-              label: 'Select 选择器'
-            }, {
-              value: 'slider',
-              label: 'Slider 滑块'
-            }, {
-              value: 'time-picker',
-              label: 'TimePicker 时间选择器'
-            }, {
-              value: 'upload',
-              label: 'Upload 上传'
-            }, {
-              value: 'rate',
-              label: 'Rate 评分'
-            }, {
-              value: 'form',
-              label: 'Form 表单'
-            }]
-          }, {
-            value: 'data',
-            label: 'Data',
-            children: [{
-              value: 'table',
-              label: 'Table 表格'
-            }, {
-              value: 'tag',
-              label: 'Tag 标签'
-            }, {
-              value: 'pagination',
-              label: 'Pagination 分页'
-            }, {
-              value: 'badge',
-              label: 'Badge 标记'
-            }]
-          }, {
-            value: 'notice',
-            label: 'Notice',
-            children: [{
-              value: 'alert',
-              label: 'Alert 警告'
-            }, {
-              value: 'message-box',
-              label: 'MessageBox 弹框'
-            }, {
-              value: 'notification',
-              label: 'Notification 通知'
-            }]
-          }, {
-            value: 'navigation',
-            label: 'Navigation',
-            children: [{
-              value: 'menu',
-              label: 'NavMenu 导航菜单'
-            }, {
-              value: 'dropdown',
-              label: 'Dropdown 下拉菜单'
-            }, {
-              value: 'steps',
-              label: 'Steps 步骤条'
-            }]
-          }, {
-            value: 'others',
-            label: 'Others',
-            children: [{
-              value: 'dialog',
-              label: 'Dialog 对话框'
-            }, {
-              value: 'collapse',
-              label: 'Collapse 折叠面板'
-            }]
-          }]
-        }, {
-          value: 'ziyuan',
-          label: '资源',
-          children: [{
-            value: 'axure',
-            label: 'Axure Components'
-          }, {
-            value: 'sketch',
-            label: 'Sketch Templates'
-          }, {
-            value: 'jiaohu',
-            label: '组件交互文档'
-          }]
-        }],
         loading: false,
         form: {
-          keyword: "",
-          status: "",
+          keyword: ""
         },
         rules: {
           keyword: [
@@ -283,35 +133,28 @@
             { required: true, message: "请选择状态", trigger: "change" },
           ],
         },
-        viiewFormRules: {
+        creatFormRules: {
           name: [
-            { required: false, message: "作品名称关键字", trigger: "blur" },
+            { required: true, message: "请输入项目名称", trigger: "blur" },
+            { min: 1, max: 10, message: '长度在 1 到 10 个字符', trigger: 'blur' }
           ],
-          desc: [
-            { required: true, message: "请填写描述", trigger: "blur" },
+          scale: [
+            { required: true, message: "请选择画幅比例", trigger: "change" }
           ],
-          label: [
-            { required: true, message: "请选择标签", trigger: "change" },
-          ],
-          entity1: [
-            { required: true, message: "请选择实体", trigger: "change" },
-          ],
-          entity2: [
-            { required: true, message: "请选择实体", trigger: "change" },
-          ],
-          // entity3: [
-          //   { required: true, message: "请选择实体", trigger: "change" },
-          // ],
+          size: [
+            { required: true, message: "请选择分辨率", trigger: "change" }
+          ]
         },
-        isShow: false,
-        viiewForm: {
+        isShow: false,   
+        isDelete: false, //删除弹窗
+        creatForm: {
           name: '',
-          desc: '',
-          label: '',
-          entity1: '',
-          entity2: '',
-          entity3: []
+          scale: '',
+          size: ''
         },
+        scaleList: [], //画幅比例列表
+        sizeList: [],  //分辨率列表
+        statusList: [], //状态列表
         selectData: [{
           value: '1',
           label: '合成中'
@@ -334,10 +177,15 @@
           value: '7',
           label: '已发布'
         }],
-        status: '',
-        value: '',
-        searchValue: '',
         currentPage: 5,
+        page: {
+          pageNo: 1,
+          pageSize: 30,
+          // map: {
+          //   resolution: 540,
+          //   wh_ratio: 16:9
+          // }
+        },
         tableData: [{
           date: 'Name1Name1Name1',
           name: 'abc@moviebook.cn',
@@ -385,45 +233,133 @@
     methods: {
       handleClose() {
         this.isShow = false;
+        this.isDelete = false;
       },
-      // 级联选择器
-      handleChange(value) {
-        console.log(value);
+      
+      // 删除项目
+      deleteProject(){
+        this.isDelete = true;
+
+        delProject().then(res => {
+          if (res.status == 1) {
+             this.$message({
+              type: "success",
+              message: res.msg
+            })
+            this.getChoicesList();
+          } else {
+            this.$message({
+              type: "error",
+              message: res.msg
+            })
+          }
+        }).catch(error => {
+          console.log(error);
+        })
+      },
+      
+      // 获取画幅，分辨率列表
+      getChoicesList(){
+        getChoicesList().then(res => {
+          if (res.status == 1) {
+            this.scaleList = res.resolution;
+            this.sizeList = res.wh_ratio;
+            this.statusList = res.status;
+          } else {
+            this.$message({
+              type: "error",
+              message: res.msg
+            });
+          }
+        }).catch(error => {
+          console.log(error);
+        })
+      },
+      // 选中项目
+      handleSelectionChange(val) {
+        this.multipleSelection = val;
+        console.log(val, 'valvalval');
+        this.getChoicesList();
+      },
+      // 清空搜索条件
+      resetSearch(){
+        this.getChoicesList();
       },
 
-      // 提交审核表单
-      onConfirm(){
-        this.$refs['viiewForm'].validate((valid) => {
+      // 创建项目
+      addProject(){
+        this.$refs['creatForm'].validate((valid) => {
           if (!valid) {
-            alert("false!");
             return false;
           }
-          // 111
-            console.log("error submit!!");
+          let params = {
+            title: this.creatForm.name,
+            resolution: this.creatForm.scale,
+            wh_ratio: this.creatForm.size
+          }
+          createProject(params).then(res => {
+            if(res.status == 1){
+              this.$message({
+                type: "success",
+                message: res.msg
+              });
+              this.tableData = res.data.xx;
+            }else{
+              this.$message({
+                type: "error",
+                message: res.msg
+              });
+            }
+          }).catch(error => {
+            console.log(error);
+          })
         });
       },
 
       onReview(){
         this.isShow = true;
       },
-      onEdit(){},
-      omDelete(){},
-      onDownload(){},
-      submitForm(formName) {
-        this.$refs[formName].validate((valid) => {
-          if (!valid) {
-            console.log("error submit!!");
-            return false;
+      
+      // 获取项目列表
+      getProjectList(){
+        const params = {
+          pageNo: this.page.pageNo,
+          pageSize: this.page.pageSize,
+          map: {
+            // search
+            ...this.form
+          },
+        };
+        getProjectList().then(res => {
+          if(res.status == 1){
+            const {pageNo,
+              pageSize,
+              recordCount,
+              pageCount} = res.fsp
+            this.page = {
+              pageNo,
+              pageSize,
+              // recordCount,
+              // pageCount,
+            }
+            this.tableData = res.datas
+          }else{
+            this.$message({
+              type: "error",
+              message: res.msg
+            })
           }
-          // 请求接口
-          console.log(this.form, "form");
-        });
+        }).catch(error => {
+          console.log(error);
+        })
       },
       handleSizeChange(val) {
         console.log(`每页 ${val} 条`);
+        this.getProjectList();
       },
       handleCurrentChange(val) {
         console.log(`当前页: ${val}`);
+        this.getProjectList();
       }
     },
   }
@@ -437,4 +373,10 @@
     display: inline-block;
     width: 220px;
   }
+  .creat-dialog {
+    .el-select {
+        width: 100%;
+      }
+  }
+ 
 </style>
