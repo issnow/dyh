022e816ba81a057b150e22bd83c058635dbce087
li.weifeng <weifeng_li@moviebook.cn>
2021-05-25 17:41:06 +0800

    审核接口联调

--- src/views/view/viewDetail.vue
+++ src/views/view/viewDetail.vue
@@ -5,15 +5,20 @@
       <ul class="list">
         <li>
           <div class="label">名称:</div>
-          <div class="content">1111</div>
+          <div class="content">{{product.title}}</div>
         </li>
         <li>
           <div class="label">描述:</div>
-          <div class="content">显示成品描述的内容</div>
+          <div class="content">{{product.description}}</div>
         </li>
         <li>
           <div class="label">标签:</div>
-          <div class="content">显示成品描述的内容</div>
+          <div class="content">
+            {{tagIds}}
+            <span>
+            {{tagName}}
+            </span>
+          </div>
         </li>
         <li>
           <div class="label">一级实体:</div>
@@ -25,7 +30,7 @@
         </li>
       </ul>
       <div class="video-play-area">
-        <videoPlay :src='getImg("movie.mp4")'></videoPlay>
+        <videoPlay :src='product.url'></videoPlay>
       </div>
     </div>
     <div class="work-detail-info">
@@ -81,12 +86,12 @@
           </el-input>
           <!-- <div class="tip">上限50个字符。</div> -->
         </el-form-item>
-        <el-form-item label="建议:" prop="name">
+        <el-form-item label="建议:" prop="audit_note">
           <el-input
             type="textarea"
             :rows="3"
             placeholder="请输入内容"
-            v-model="form.name"
+            v-model="form.audit_note"
             maxlength="50"
             show-word-limit
           >
@@ -95,10 +100,10 @@
         </el-form-item>
       </el-form>
       <div class="foot-btn">
-        <el-button type="primary" @click="submitForm('form')" :loading="loading"
+        <el-button type="primary" @click="submitForm(1)" :loading="loading"
           >审核通过</el-button
         >
-        <el-button @click="onCancel">审核未通过</el-button>
+        <el-button @click="submitForm(2)">审核未通过</el-button>
       </div>
     </div>
   </div>
@@ -106,6 +111,13 @@
 
 <script>
 import videoPlay from "./videoPlay";
+import { 
+  getAuditDetail,
+  getAudit,
+  getTagList,
+  getEntityList
+} from "@api/project";
+
 export default {
   components: {
     videoPlay,
@@ -113,34 +125,30 @@ export default {
   data() {
     return {
       loading: false,
-      // query: {}
       form: {
-        description: "",
-        name: "",
-        label: "选项1",
-        thing: "",
-        thing2: "",
+        code: this.$route.query.code,
+        audit_note: "",
+        audit_status: '',
+        // name: "",
+        // label: "选项1",
+        // thing: "",
+        // thing2: "",
       },
       rules: {
         name: [
-          { required: true, message: "请输入", trigger: "blur" },
-          // { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" },
+          { required: false, message: "请输入", trigger: "blur" }
         ],
         label: [
-          { required: true, message: "请输入", trigger: "blur" },
-          // { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" },
+          { required: false, message: "请输入", trigger: "blur" }
         ],
         thing: [
-          { required: true, message: "请输入", trigger: "blur" },
-          // { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" },
+          { required: false, message: "请输入", trigger: "blur" }
         ],
         thing2: [
-          { required: true, message: "请输入", trigger: "blur" },
-          // { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" },
+          { required: false, message: "请输入", trigger: "blur" }
         ],
         description: [
-          { required: true, message: "请输入", trigger: "blur" },
-          // { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" },
+          { required: false, message: "请输入", trigger: "blur" }
         ],
       },
       options: [
@@ -165,7 +173,20 @@ export default {
           label: "北京烤鸭",
         },
       ],
+      product: {},
+      task: {},
+      tagIds: [],
+      tagList: [],
+      entityList: [],
+      tagName: [],
+      entityName: []
     };
+  },
+  created(){
+    this.getTagList();
+  },
+  mounted() {
+    this.getAuditDetail();
   },
   methods: {
     getImg(src) {
@@ -173,42 +194,100 @@ export default {
         return require("@/assets/" + src);
       }
     },
-    submitForm(formName) {
-      this.$refs[formName].validate(async (valid) => {
-        if (valid) {
-          console.log(this.form);
-          this.loading = true;
+    // 获取标签列表
+    getTagList(){
+      getTagList().then(res => {
+        if(res.status == 1){
+          this.tagList = res.element;
+          this.product.tag_ids;
+        }else{
+          this.$message({
+            type: "error",
+            message: res.msg
+          })
+        }
+      }).catch(error => {
+        console.log(error);
+      })
+    },
+    // 提交审核
+    submitForm(val) {
+      this.form.audit_status = val;
+      this.loading = true;
 
+      this.$refs['form'].validate(async (valid) => {
+        if (valid) {
+          const params = {
+            ...this.form
+          }
           this.$confirm("确认该作品相关信息符合规范，审核通过?", "审核确认", {
             confirmButtonText: "提交",
             cancelButtonText: "取消",
             type: "warning",
-          })
-            .then(() => {
-              this.$message({
-                type: "success",
-                message: "提交成功!",
-              });
-            })
-            .catch(() => {
-              this.$message({
-                type: "info",
-                message: "已取消",
-              });
+          }).then(() => {
+            getAudit(params).then(res => {
+              if(res.status == 1){
+                this.loading = false;
+                this.$message({
+                  type: "success",
+                  message: res.msg
+                });
+              }else{
+                this.$message({
+                  type: "error",
+                  message: res.msg
+                })
+              }
+            }).catch(error => {
+              console.log(error);
             });
-          setTimeout(() => {
-            this.loading = false;
-          }, 2000);
+
+          }).catch(() => {
+            this.$message({
+              type: "info",
+              message: "已取消",
+            });
+          });
         } else {
-          console.log("error submit!!");
           return false;
         }
       });
     },
-    onCancel() {
-      this.$router.push('/workManager')
-      this.$refs.form.resetFields()
+
+    // 获取审核列表
+    getAuditDetail(){
+      const params = {
+        code: this.$route.query.code
+      };
+
+      getAuditDetail(params).then(res => {
+        if(res.status == 1){
+          this.product = res.element && res.element.product;
+          this.task = res.element && res.element.task;
+
+          this.tagIds = this.product.tag_ids;
+          const tagList = this.product.tag_ids;
+          for(let i = 0, len = this.tagList; i < len; i++){
+            for(let j = 0, length = tagList; j < length; j++){
+              if(this.tagList[i] == Number(tagList[j])){
+                this.tagName.push(this.tagList[i].name);
+                console.log(this.tagName, 'this.tagName')
+              }
+
+            }
+          }
+          // console.log(this.product , this.task, 'result');
+        }else{
+          this.$message({
+            type: "error",
+            message: res.msg
+          })
+        }
+      }).catch(error => {
+        console.log(error);
+      })
     },
+
   },
 };
 </script>
