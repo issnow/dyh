a10566e532443787a3b8a12e4f15bb49fa9b09d5
issnow <issnow.guo@gmail.com>
2021-05-27 10:38:44 +0800

    feat: axios去console

--- src/api/request.js
+++ src/api/request.js
@@ -33,7 +33,6 @@ instance.interceptors.request.use(function (config) {
 
 // 添加响应拦截器
 instance.interceptors.response.use(function (response) {
-  console.log(response, 'response');
   const {data} = response
   if(data.status == '-101' ){
     // 用户未登录
@@ -42,7 +41,6 @@ instance.interceptors.response.use(function (response) {
   // 对响应数据做点什么
   return response.data;
 }, function (error) {
-  console.log(error, 'err');
   // 对响应错误做点什么
   // return Promise.reject(error);
   const res = error.response
