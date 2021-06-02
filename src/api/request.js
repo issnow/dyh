04293e85893802280d0cfee42a4a0a2f30cdaf7e
issnow <issnow.guo@gmail.com>
2021-06-02 10:27:53 +0800

    feat: 去掉Authorization

--- src/api/request.js
+++ src/api/request.js
@@ -30,12 +30,12 @@ instance.interceptors.request.use(function (config) {
   //     'Content-Type': 'application/x-www-form-urlencoded'
   //   }
   // }
-  if (sessionStorage.getItem('token')) {
-    config.headers = {
-      ...config.headers,
-      Authorization: sessionStorage.getItem('token')
-    }
-  }
+  // if (sessionStorage.getItem('token')) {
+  //   config.headers = {
+  //     ...config.headers,
+  //     Authorization: sessionStorage.getItem('token')
+  //   }
+  // }
   // 在发送请求之前做些什么
   return config;
 }, function (error) {
