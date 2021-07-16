f4e0b0f6f1537a1491b7b5c03b922f14d99aec2f
issnow <issnow.guo@gmail.com>
2021-07-16 17:55:00 +0800

    feat: 设置api地址

--- src/api/request.js
+++ src/api/request.js
@@ -3,7 +3,7 @@ import {
   Message
 } from 'element-ui'
 // export const baseURL = 'http://172.18.20.78/dayunhe/backend/public'
-export const baseURL = 'http://123.60.24.237:8085'
+export const baseURL = process.env.NODE_ENV == 'development' ? 'http://123.60.24.237:8085' : 'https://api-dyh.videoyi.com'
 
 const axios = require('axios')
 let instance = axios.create({
