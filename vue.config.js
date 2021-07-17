373ad8a954f9209c9bf6b536d439f4ba5ac37946
issnow <issnow.guo@gmail.com>
2021-07-17 10:47:37 +0800

    feat: 去掉publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 module.exports = {
-  publicPath: '/m/',
-  outputDir: path.resolve(__dirname, 'dist/m'),
+  // publicPath: '/m/',
+  // outputDir: path.resolve(__dirname, 'dist/m'),
   // configureWebpack: {
   //   plugins: [
   //     // new MyAwesomeWebpackPlugin()
