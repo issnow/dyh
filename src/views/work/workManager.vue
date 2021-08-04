5055ed453730b2b32b2dbbdec14ae09546410d62
issnow <issnow.guo@gmail.com>
2021-08-04 14:17:52 +0800

    feat: 作品合成中合成失败都不显示下载

--- src/views/work/workManager.vue
+++ src/views/work/workManager.vue
@@ -225,7 +225,10 @@
             @click="onWatch(scope.row.code)"
             >查看</el-button
           >
-          <el-button type="text" @click="downloadFile(scope.row)"
+          <el-button
+            v-if="![1, 2].includes(scope.row.status)"
+            type="text"
+            @click="downloadFile(scope.row)"
             >下载</el-button
           >
           <el-button
